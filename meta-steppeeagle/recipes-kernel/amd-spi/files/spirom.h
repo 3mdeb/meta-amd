3fb21abe099f74b73c3341a96f0adef2f7198ef5
Arindam Nath <arindam.nath@amd.com>
2014-08-21 18:13:14 +0530

    yocto: meta-amd: make sure spirom.h is same across app and driver
    spirom.h header file should be same for both the test app and the
    SPI client driver, since they refer the same IOCTL structure and
    to driver directory.

--- meta-steppeeagle/recipes-kernel/amd-spi/files/spirom.h
+++ meta-steppeeagle/recipes-kernel/amd-spi/files/spirom.h
@@ -39,5 +39,15 @@ struct spi_ioc_transfer {
 		? ((N)*(sizeof (struct spi_ioc_transfer))) : 0)
 #define SPI_IOC_MESSAGE(N) _IOW(SPI_IOC_MAGIC, 0, char[SPI_MSGSIZE(N)])
 
+/* SPI ROM command codes */
+#define ROM_WREN		0x06
+#define ROM_WRDI		0x04
+#define ROM_RDSR		0x05
+#define ROM_RDID		0x9F
+#define ROM_CHIP_ERASE		0x60
+#define ROM_SECTOR_ERASE	0x20
+#define ROM_BLOCK_ERASE		0xD8
+#define ROM_READ		0x03
+#define ROM_WRITE		0x02
 
 #endif /* SPIROM_H */
